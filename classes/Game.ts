ef01962837d33eb94192c45457df9b5d82b17cbe
Ian Webster <87659522+devianweb@users.noreply.github.com>
2025-10-27 21:27:39 +0000

    refactored (#18)
    Co-authored-by: carrcig <87659522+carrcig@users.noreply.github.com>

---
+++ classes/Game.ts
@@ -0,0 +1,214 @@
+import * as THREE from "three";
+import Player from "./Player";
+import Controls from "./Controls";
+import { InputMessage, ServerMessage } from "../types";
+import {
+  handleAuthoritativeUpdate,
+  handlePositionUpdate,
+} from "../utils/ws-utils";
+import Mouse from "./Mouse";
+import Camera from "./Camera";
+import Pointer from "./Pointer";
+
+export default class Game {
+  // threejs
+  renderer: THREE.WebGLRenderer;
+  scene: THREE.Scene;
+
+  // game
+  userId: string;
+  player1: Player;
+  players: Map<string, Player>;
+  controls: Controls;
+  mouse: Mouse;
+  camera: Camera;
+  pointer: Pointer;
+  lastUpdate = performance.now();
+  timestep = 1000 / 60;
+
+  // networking
+  ws: WebSocket;
+  tick = 0;
+  seqId = 0;
+  sendFinalInputTick = false;
+  messages: ServerMessage[] = [];
+
+  // ui
+  // coordinates
+  coordinates = document.getElementById("coordinates") as HTMLDivElement;
+  mouseCoordinates = document.getElementById(
+    "mouse-coordinates"
+  ) as HTMLDivElement;
+  // status
+  status = document.getElementById("status") as HTMLDivElement;
+
+  constructor(
+    renderer: THREE.WebGLRenderer,
+    scene: THREE.Scene,
+    userId: string,
+    player1: Player,
+    players: Map<string, Player>,
+    controls: Controls,
+    mouse: Mouse,
+    camera: Camera,
+    pointer: Pointer,
+    ws: WebSocket
+  ) {
+    this.renderer = renderer;
+    this.scene = scene;
+    this.userId = userId;
+    this.player1 = player1;
+    this.players = players;
+    this.controls = controls;
+    this.mouse = mouse;
+    this.camera = camera;
+    this.pointer = pointer;
+    this.ws = ws;
+  }
+
+  start = () => {
+    this.createBackground();
+    this.setupWebSocketHandlers();
+
+    requestAnimationFrame(this.gameLoop);
+    this.renderer.setAnimationLoop(this.renderLoop);
+  };
+
+  gameLoop = () => {
+    const now = performance.now();
+    let dt = now - this.lastUpdate;
+
+    while (dt >= this.timestep) {
+      // console.log(json);
+      this.messages.forEach((json) => {
+        if (json.type === "POSITION") {
+          handlePositionUpdate(json, this.players, this.scene);
+        } else if (json.type === "AUTHORITATIVE") {
+          handleAuthoritativeUpdate(
+            json,
+            this.players,
+            this.scene,
+            this.userId
+          );
+        }
+      });
+      while (this.messages.length > 0) this.messages.pop();
+
+      //mouse bits
+      this.mouse.update();
+
+      this.player1.updatePlayerPosition();
+      this.pointer.update();
+      this.camera.updateCameraPosition(this.player1);
+      this.updateServer();
+
+      dt -= this.timestep;
+      this.lastUpdate += this.timestep;
+      if (this.tick % 600 === 0) {
+        console.log(this.players);
+        console.log(this.camera);
+      }
+
+      this.coordinates.textContent = `${this.player1.position.x.toFixed(
+        2
+      )}, ${this.player1.position.y.toFixed(2)}`;
+      this.mouseCoordinates.textContent = `${this.mouse.worldPoint.x.toFixed(
+        2
+      )}, ${this.mouse.worldPoint.y.toFixed(2)}`;
+
+      this.tick++;
+    }
+
+    requestAnimationFrame(this.gameLoop);
+  };
+
+  renderLoop = () => {
+    this.players.forEach((p) => {
+      p.mesh.position.lerp(p.position, 0.3);
+    });
+
+    this.renderer.render(this.scene, this.camera.camera);
+  };
+
+  updateServer = () => {
+    if (this.ws.readyState === WebSocket.OPEN) {
+      if (
+        this.controls.inputs.up ||
+        this.controls.inputs.down ||
+        this.controls.inputs.left ||
+        this.controls.inputs.right ||
+        this.sendFinalInputTick
+      ) {
+        this.sendFinalInputTick = true;
+        const message: InputMessage = {
+          userId: this.userId,
+          type: "INPUT",
+          seqId: this.seqId,
+          payload: this.controls.inputs,
+        };
+        this.ws.send(JSON.stringify(message));
+        this.controls.updateHistory(this.seqId);
+        this.seqId++;
+        if (
+          !this.controls.inputs.up &&
+          !this.controls.inputs.down &&
+          !this.controls.inputs.left &&
+          !this.controls.inputs.right
+        )
+          this.sendFinalInputTick = false;
+      }
+    }
+  };
+
+  createBackground = () => {
+    // background
+    const geometry = new THREE.BufferGeometry();
+    const vertices: number[] = [];
+
+    for (let i = 0; i < 10000; i++) {
+      vertices.push(THREE.MathUtils.randFloatSpread(100));
+      vertices.push(THREE.MathUtils.randFloatSpread(100));
+      vertices.push(-1);
+    }
+
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(vertices, 3)
+    );
+
+    const particles = new THREE.Points(
+      geometry,
+      new THREE.PointsMaterial({ color: 0x888888 })
+    );
+    this.scene.add(particles);
+  };
+
+  setupWebSocketHandlers = () => {
+    this.ws.onopen = () => {
+      this.status.textContent = "Connected to server";
+      this.status.style.color = "green";
+    };
+
+    this.ws.onerror = (error: Event) => {
+      // WebSocket error is broadly typed; narrow if needed later
+      this.status.textContent = "Error";
+      this.status.style.color = "red";
+      console.error(error);
+    };
+
+    this.ws.onclose = () => {
+      this.status.textContent = "Disconnected from server";
+      this.status.style.color = "red";
+    };
+
+    this.ws.onmessage = (event: MessageEvent<string>) => {
+      try {
+        const json: ServerMessage = JSON.parse(event.data);
+        // console.log(json);
+        this.messages.push(json);
+      } catch (e) {
+        console.warn("Received malformed message", e);
+      }
+    };
+  };
+}
--- main.ts
+++ main.ts
@@ -1,15 +1,10 @@
 import * as THREE from "three";
 import Player from "./classes/Player";
-import {
-  handlePositionUpdate,
-  handleAuthoritativeUpdate,
-} from "./utils/ws-utils";
-import { ServerMessage } from "./types";
 import Controls from "./classes/Controls";
 import Camera from "./classes/Camera";
-import { InputMessage } from "./types";
 import Mouse from "./classes/Mouse";
 import Pointer from "./classes/Pointer";
+import Game from "./classes/Game";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
@@ -42,156 +37,23 @@ window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// background
-const geometry = new THREE.BufferGeometry();
-const vertices: number[] = [];
-
-for (let i = 0; i < 10000; i++) {
-  vertices.push(THREE.MathUtils.randFloatSpread(100));
-  vertices.push(THREE.MathUtils.randFloatSpread(100));
-  vertices.push(-1);
-}
-
-geometry.setAttribute(
-  "position",
-  new THREE.Float32BufferAttribute(vertices, 3)
-);
-
-const particles = new THREE.Points(
-  geometry,
-  new THREE.PointsMaterial({ color: 0x888888 })
-);
-scene.add(particles);
-
-// coordinates
-const coordinates = document.getElementById("coordinates") as HTMLDivElement;
-const mouseCoordinates = document.getElementById(
-  "mouse-coordinates"
-) as HTMLDivElement;
-
-// logic loops
-let lastUpdate = performance.now();
-const timestep = 1000 / 60;
-let tick = 0;
-let seqId = 0;
-let sendFinalInputTick = false;
-let messages: ServerMessage[] = [];
-
-function gameLoop() {
-  const now = performance.now();
-  let dt = now - lastUpdate;
-
-  while (dt >= timestep) {
-    // console.log(json);
-    messages.forEach((json) => {
-      if (json.type === "POSITION") {
-        handlePositionUpdate(json, players, scene);
-      } else if (json.type === "AUTHORITATIVE") {
-        handleAuthoritativeUpdate(json, players, scene, userId);
-      }
-    });
-    while (messages.length > 0) messages.pop();
-
-    //mouse bits
-    mouse.update();
-
-    player1.updatePlayerPosition();
-    pointer.update();
-    camera.updateCameraPosition(player1);
-    updateServer();
-
-    dt -= timestep;
-    lastUpdate += timestep;
-    if (tick % 600 === 0) {
-      console.log(players);
-      console.log(camera);
-    }
-
-    coordinates.textContent = `${player1.position.x.toFixed(
-      2
-    )}, ${player1.position.y.toFixed(2)}`;
-    mouseCoordinates.textContent = `${mouse.worldPoint.x.toFixed(
-      2
-    )}, ${mouse.worldPoint.y.toFixed(2)}`;
-
-    tick++;
-  }
-
-  requestAnimationFrame(gameLoop);
-}
-
-function updateServer() {
-  if (ws.readyState === WebSocket.OPEN) {
-    if (
-      controls.inputs.up ||
-      controls.inputs.down ||
-      controls.inputs.left ||
-      controls.inputs.right ||
-      sendFinalInputTick
-    ) {
-      sendFinalInputTick = true;
-      const message: InputMessage = {
-        userId: userId,
-        type: "INPUT",
-        seqId: seqId,
-        payload: controls.inputs,
-      };
-      ws.send(JSON.stringify(message));
-      controls.updateHistory(seqId);
-      seqId++;
-      if (
-        !controls.inputs.up &&
-        !controls.inputs.down &&
-        !controls.inputs.left &&
-        !controls.inputs.right
-      )
-        sendFinalInputTick = false;
-    }
-  }
-}
-
-function renderLoop() {
-  players.forEach((p) => {
-    p.mesh.position.lerp(p.position, 0.3);
-  });
-
-  renderer.render(scene, camera.camera);
-}
-
 // WebSockets
-const status = document.getElementById("status") as HTMLDivElement;
-
 // Connect to the WebSocket server
 const ws = new WebSocket(
   `wss://semiglazed-too-kimberlie.ngrok-free.dev/ws/games/1337?userId=${userId}`
 );
 
-ws.onopen = () => {
-  status.textContent = "Connected to server";
-  status.style.color = "green";
-};
+const game = new Game(
+  renderer,
+  scene,
+  userId,
+  player1,
+  players,
+  controls,
+  mouse,
+  camera,
+  pointer,
+  ws
+);
 
-ws.onerror = (error: Event) => {
-  // WebSocket error is broadly typed; narrow if needed later
-  status.textContent = "Error";
-  status.style.color = "red";
-  console.error(error);
-};
-
-ws.onclose = () => {
-  status.textContent = "Disconnected from server";
-  status.style.color = "red";
-};
-
-ws.onmessage = (event: MessageEvent<string>) => {
-  try {
-    const json: ServerMessage = JSON.parse(event.data);
-    // console.log(json);
-    messages.push(json);
-  } catch (e) {
-    console.warn("Received malformed message", e);
-  }
-};
-
-requestAnimationFrame(gameLoop);
-renderer.setAnimationLoop(renderLoop);
+game.start();
